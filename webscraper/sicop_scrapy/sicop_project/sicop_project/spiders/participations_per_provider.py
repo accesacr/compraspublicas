dfb123eaaf999c9727882b3cb5a1540a9a51a70c
Jose Lidier Artavia <lidiera@gmail.com>
2021-05-16 17:45:16 -0600

    participations crawler code finished but not tested, ready for a test run

--- webscraper/sicop_scrapy/sicop_project/sicop_project/spiders/participations_per_provider.py
+++ webscraper/sicop_scrapy/sicop_project/sicop_project/spiders/participations_per_provider.py
@@ -19,17 +19,14 @@ class ParticipationsPerProviderSpider(scrapy.Spider):
     #    'LOG_LEVEL': 'INFO'
     #}
     def start_requests(self):
-        provider_id_list = [
-            "3101182897"          
-        ]
-#        import csv
-#        category_id_list = []
-#        with open('/usr/src/app/sicop_project/sicop_project/crawled_data/wgscategories_level4_onlyid_noheader.csv','rt', encoding='utf-16') as csvfile: 
-#            reader = csv.reader(csvfile, delimiter=',', quotechar='"') 
-#            for row in reader:
-#                #print(', '.join(row))
-#                #logging.info(row[0])
-#                category_id_list.append(row[0])
+        import csv
+        provider_id_list = []
+        with open('/usr/src/app/sicop_project/sicop_project/other_data/all_providers_onlyid_noheaders.csv','rt', encoding='utf-16') as csvfile: 
+            reader = csv.reader(csvfile, delimiter=',', quotechar='"') 
+            for row in reader:
+                #print(', '.join(row))
+                #logging.info(row[0])
+                provider_id_list.append(row[0])
         for provider_id in provider_id_list:
             # logging.info(category_id)
             url = "https://www.sicop.go.cr/moduloBid/report/EP_REJ_COQ717.jsp?isQuery=Y&biz_reg_no="
@@ -46,14 +43,20 @@ class ParticipationsPerProviderSpider(scrapy.Spider):
             else: 
                 values = []
                 for column in row.css('td'):
-                    value = column.css('::text').get()
-                    if value is None:
-                        value = ''
-                    values.append(value)
-                logging.info(values)
-                if values[0].strip() != 'Los datos consultados':
+                    #value = column.css('::text').get()
+                    #if value is None:
+                    concat_value = ''
+                    value_arr = column.css('::text').getall()
+                    if value_arr != []:
+                        for value in value_arr:
+                            concat_value += value.replace('\n', '|').replace('\t', '|').replace('\r', '|')
+                    values.append(concat_value)
+                #logging.info(values)
+                if values[0].strip().find('Los datos consultados') == -1:
                     yield {
                         'provider_id' : provider_id
+                        ,'procedure_num' : values[0].split('|', 1)[0].strip()
+                        ,'institution' : values[0].split('|', 1)[1].strip()
                         ,'procedure_num_and_institution' : values[0].strip()
                         ,'budget_item' : values[1].strip()
                         ,'procedure_description' : values[2].strip()
@@ -63,4 +66,7 @@ class ParticipationsPerProviderSpider(scrapy.Spider):
                     }
 """                        from scrapy.shell import inspect_response
         inspect_response(response, self)
+                provider_id_list = [
+            "31011828970"          
+        ]
 """
